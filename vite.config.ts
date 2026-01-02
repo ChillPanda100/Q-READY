209a501fc180c6074e70a36390c91cf9b4f37720
ChillPanda100 <xyzhao123b@Gmail.com>
2026-01-02 13:14:52 -0500

    Redo configuring for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const base = process.env.VITE_BASE ?? './'
+
 export default defineConfig({
-  base: '/Q-READY/',
+  base,
   plugins: [react()],
 })
